47908e5276694952d3a6da0013c29ad4a412e663
Mart-038 <mstukje@gmail.com>
2026-05-11 13:11:53 +0200

    Smoke test

--- src/test/java/nl/miwnn/ch19/binarybros/brobook/BrobookApplicationTests.java
+++ src/test/java/nl/miwnn/ch19/binarybros/brobook/BrobookApplicationTests.java
@@ -2,8 +2,10 @@ package nl.miwnn.ch19.binarybros.brobook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BrobookApplicationTests {
 
 	@Test
